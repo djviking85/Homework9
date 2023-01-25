a791ddfd785dae925081c067220fd538303095e7
Viking <djviking@yandex.ru>
2023-01-25 13:50:53 +0300

    Homework9 т2

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
     public static void main(String[] args) {
         task1();
         task2();
@@ -8,12 +16,29 @@ public class Main {
 
     public static void task1 () {
         System.out.println("Задача 1");
-         // Пишем код для задачи 1
+        // Бухгалтеры попросили посчитать сумму всех выплат за месяц.
+        //
+        //Напишите программу, которая решит эту задачу, и выведите в
+        // консоль результат в формате: «Сумма трат за месяц составила … рублей».
+        int[] arr = generateRandomArray();
+        int summaAll = 0;
+        for (int i = 0; i < arr.length; i++) {
+          summaAll += arr[i];
+        }
+        System.out.println(" Сумма трат за месяц составила - " +summaAll+ " рублей");
+
     }
 
     public static void task2 () {
         System.out.println("Задача 2");
-         // Пишем код для задачи 2
+        int[] arr = generateRandomArray();
+        int summaAll = 0;
+        for (int i = 0; i < arr.length; i++) {
+            summaAll += arr[i];
+        }
+        System.out.println(" Сумма трат за месяц составила - " +summaAll+ " рублей");
+
+
     }
     public static void task3 () {
         System.out.println("Задача 3");
